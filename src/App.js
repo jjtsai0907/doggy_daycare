c4fc4ecb31447b8d3543737d816566248fee4d2a
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-04-30 17:13:43 +0800

    changed to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
 import logo from './doggylogo.png';
 import './App.css';
 import WelcomeView from './components/WelcomeView';
