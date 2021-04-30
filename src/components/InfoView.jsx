e108f12b33fbac206d77d74c89e6d96ff42815e7
jia.jiuan.tsai <jia.jiuan.tsai@iths.se>
2021-04-30 16:48:13 +0800

    deploy

--- src/components/InfoView.jsx
+++ src/components/InfoView.jsx
@@ -5,44 +5,44 @@ const InfoView = ({ dataLoaded, getDogData, selectedDog }) => {
 
     console.log({ selectedDog });
     let ifAtHome = "";
-    if (selectedDog.present){
+    if (selectedDog.present) {
         ifAtHome = "at home"
     } else {
         ifAtHome = "not at home"
     }
 
 
-
-    return (
-        <div className="infoV">
-
-            <img src={selectedDog.img} alt="dog image" />
-
-            <div >
-                <h1 className="infoV h1">
-                    Hello! I'm {selectedDog.name} and {ifAtHome} right now!
-                </h1>
-            </div>
-
-            <div  className="infoV grid">
-                
-                <h2 >Sex: {selectedDog.sex}</h2>
-                <h2 >Breed: {selectedDog.breed}</h2>
-                
-                <h2 >Chip Number: {selectedDog.chipNumber}</h2>
-                <h2 >My owner: {selectedDog.owner.name} {selectedDog.owner.lastName}</h2>
-                
-                <h2 >Phone Number: {selectedDog.owner.phoneNumber}</h2>
-            </div>
-
+        return (
             
-                
             
-        </div>
 
-    )
+            <div className="infoV">
+    
+                <img src={selectedDog.img} alt="dog image" />
+    
+                <div >
+                    <h1 className="infoV h1">
+                        Hello! I'm {selectedDog.name} and {ifAtHome} right now!
+                    </h1>
+                </div>
+    
+                <div  className="infoV grid">
+                    
+                    <h2 >Sex: {selectedDog.sex}</h2>
+                    <h2 >Breed: {selectedDog.breed}</h2>
+                    
+                    <h2 >Chip Number: {selectedDog.chipNumber}</h2>
+                    <h2 >My owner: {selectedDog.owner.name} {selectedDog.owner.lastName}</h2>
+                    
+                    <h2 >Phone Number: {selectedDog.owner.phoneNumber}</h2>
+                </div>
+    
+            </div>
+    
 
 
+
+        )
 }
 
 
